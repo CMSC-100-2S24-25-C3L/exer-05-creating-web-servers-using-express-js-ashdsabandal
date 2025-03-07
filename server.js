5dd759279fa5e190bab2b88af88c6ef63a228d8f
ashdsabandal <adsabandal@up.edu.ph>
2025-03-07 12:28:15 +0800

    finsished server.js, test.js

--- server.js
+++ server.js
@@ -1,27 +1,100 @@
 import express from 'express';
+import { appendFileSync } from 'node:fs';
+import { readFileSync } from 'node:fs';
+
 // instantiate the server
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
-
-// this tells our app to listen for GET messages on the / path
-// the callback function specifies what the server will do when a message is received
-app.get('/', (req, res) => {
-    // console.log(req);
-    // console.log(res);
-    res.send('Received a GET request.');
+app.get('/find-by-isbn-author', (req, res) => {
+    const info = findByIsbnAuthor(req.query.isbn, req.query.author);
+    res.send(info);
     
 });
-app.get('/greeting', (req, res) => {
-    res.send('Hello' + req.query.name);
+
+app.get('/find-by-author', (req, res) => {
+    const info = findByAuthor(req.query.author);
+    res.send(info);
     
 });
-app.post('/submit-data', (req, res) => {
-    res.send('Received a POST request from ' + req.body.name);
-});
+app.post('/add-book', (req, res) => {
+    res.send({success: addBook(req.body)});
 
+});
 
 // this tells our server to listen to the port 3000
 // we can also pass an optional callback function to execute after the server starts
 app.listen(3000, () => { console.log('Server started at port 3000'); })
+
+function addBook(bookInfo) {
+
+    if ((bookInfo.name == null || bookInfo.name == "")
+    ||  (bookInfo.isbn == null || bookInfo.isbn == "")
+    ||  (bookInfo.author == null || bookInfo.author == "")
+    ||  (bookInfo.year == null || bookInfo.year == "")
+    ) {
+        console.log("Not all fields are present");
+        return false;
+    } 
+
+    try {
+        appendFileSync('books.txt', bookInfo.name + ',' + bookInfo.isbn + ',' + bookInfo.author + ',' + bookInfo.year + "\n");
+        console.log('The "data to append" was appended to file!');
+      } catch (err) {
+        /* Handle the error */
+      } 
+    
+    return true;
+}
+
+function findByIsbnAuthor(isbn, author) {
+
+    try {
+        const data = readFileSync('books.txt', 'utf-8').trim().split('\n'); 
+        for (let i = 0; i < data.length; i++) {
+            data[i] = data[i].split(',');
+            
+        }
+
+        for (let i = 0; i < data.length; i++) {
+            if (data[i][1] == isbn && data[i][2] == author){
+                return `Book Name: ${data[i][0]}\nISBN: ${data[i][1]}\nAuthor: ${data[i][2]}\nYear: ${data[i][3]}\n\n`;
+            }
+
+        }
+
+        return "No info available";
+    } catch (err) {
+        /* Handle the error */
+    }
+
+}
+
+function findByAuthor(author) {
+
+    try {
+        const data = readFileSync('books.txt', 'utf-8').trim().split('\n'); 
+        for (let i = 0; i < data.length; i++) {
+            data[i] = data[i].split(',');
+            
+        }
+
+        let info = "";
+
+        for (let i = 0; i < data.length; i++) {
+            if (data[i][2] == author){
+                info += `Book Name: ${data[i][0]}\nISBN: ${data[i][1]}\nAuthor: ${data[i][2]}\nYear: ${data[i][3]}\n\n`;
+            }
+
+        }
+
+        if (info != "") {
+            return info;
+        }
+        return "No info available";
+    } catch (err) {
+        /* Handle the error */
+    }
+
+}
--- test.js
+++ test.js
@@ -1,12 +1,60 @@
 import needle from 'needle';
 
-needle.get('http://localhost:3000/', (err, res) => {
-    console.log(res.body) // prints the body of the response message
-})
-
-needle.post('http://localhost:3000/submit-data', 
-    { name: 'Ash'},
+needle.post('http://localhost:3000/add-book', 
+    {   name: 'Harry Potter and the Philosopher’s Stone',
+        isbn: '978-0-7475-3269-9',
+        author: "J.K Rowling",
+        year: "1997"
+    },
     (err, res) => {
-    console.log(res.body) // prints the server's response 'Received a POST request.'
+    console.log(res.body) 
 })
 
+needle.post('http://localhost:3000/add-book', 
+    {   name: 'Harry Potter and the Chamber of Secrets',
+        isbn: '0-7475-3849-2',
+        author: "J.K Rowling",
+        year: "1998"
+    },
+    (err, res) => {
+    console.log(res.body) 
+})
+
+needle.post('http://localhost:3000/add-book', 
+    {   name: 'The Little Prince',
+        isbn: '978-0156012195',
+        author: "Antoine Saint-Exupery",
+        year: "1943"
+    },
+    (err, res) => {
+    console.log(res.body) 
+})
+
+//invalid input
+needle.post('http://localhost:3000/add-book', 
+    {   name: 'The Book With Empty ISBN',
+        isbn: '',
+        author: "William Shakespeare",
+        year: "2000"
+    },
+    (err, res) => {
+    console.log(res.body) 
+})
+
+//invalid input
+needle.post('http://localhost:3000/add-book', 
+    {   name: 'The Book Without ISBN',
+        author: "John Pork",
+        year: "1999"
+    },
+    (err, res) => {
+    console.log(res.body) 
+})
+
+needle.get('http://localhost:3000/find-by-isbn-author?isbn=978-0-7475-3269-9&author=J.K+Rowling', (err, res) => {
+    console.log(res.body) 
+})
+
+needle.get('http://localhost:3000/find-by-author?author=J.K+Rowling', (err, res) => {
+    console.log(res.body) 
+})
